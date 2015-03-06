16cfe1e236aa8564ec1c8cf0f4348d15db07c0de
Alex Jones <alex.theboy.jones@gmail.com>
2015-03-06 11:37:45 -0800

    starting this

---
+++ base62.js
@@ -0,0 +1,14 @@
+// uses jquery and big.js
+
+var Base62 = (function(Base62, $, undefined) {
+	
+
+	Base62.fromHex = new function(var data) {
+		
+	}
+
+
+ 
+
+	return Base62;
+}(Base62 || {}, jQuery));
